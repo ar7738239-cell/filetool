71e1598614bfca6e2036a60b34ffb9e7a6c3d1e6
ar7738239-cell <ar7738239@gmail.com>
2026-08-15 23:11:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,70 @@
-const CACHE_NAME = "adiyogitools-v2";
+const CACHE_NAME = "adiyogitools-v3";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  
+    "/",
+    "/index.html",
+    "/style.css"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+
+        fetch(event.request)
+            .then(response => {
+
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, copy);
+                });
+
+                return response;
+
+            })
+            .catch(() => {
+
+                return caches.match(event.request);
+
+            })
+
+    );
+
 });
